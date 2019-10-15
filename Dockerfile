9e91b6be6b980519291f5636a30b381f63911bc7
sigviper <sigviper@gmail.com>
2019-10-15 23:16:51 +0200

    Better visualisation

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,9 @@
 FROM python:3.6-slim-buster
 #RUN apt-get update -y
 #RUN apt-get install -y python-pip python-dev build-essential
+RUN pip install flask
 COPY . /app
 WORKDIR /app
-RUN pip install -r requirements.txt
+#RUN pip install -r requirements.txt
 ENTRYPOINT ["python"]
 CMD ["app.py"]
--- app.py
+++ app.py
@@ -1,11 +1,23 @@
-from flask import Flask
+from flask import Flask, send_from_directory
+import socket
+import os.path
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return 'Flask Dockerized v4, test webhook'
+def the_app():
+	#return app.send_static_file('tmpl/index.html')
+	#path = 'tmpl\index.html'
+	#return app.send_static_file(os.path.join('js', path).replace('\\','/')) 
+	with open('tmpl/index.html', encoding='utf-8') as tmpl:
+		return tmpl.read().format(node=this_node)
 
 
+@app.route('/tmpl/<path:path>')
+def send_static(path):
+    return send_from_directory('tmpl', path)
+
+
+this_node = socket.getfqdn()
 if __name__ == '__main__':
     app.run(debug=False, host='0.0.0.0')
