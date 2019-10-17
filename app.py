9878be1798b8c7a1781e4408faa8de1886f65e89
sigviper <sigviper@gmail.com>
2019-10-17 18:22:54 +0200

    Introduce instance id instead of FQDN, add comments in code

--- app.py
+++ app.py
@@ -1,23 +1,32 @@
 from flask import Flask, send_from_directory
 import socket
 import os.path
+import random
+import string
+
 app = Flask(__name__)
 
 
 @app.route('/')
 def the_app():
-	#return app.send_static_file('tmpl/index.html')
-	#path = 'tmpl\index.html'
-	#return app.send_static_file(os.path.join('js', path).replace('\\','/')) 
+	"""Serve index.html treating is as string template"""
 	with open('tmpl/index.html', encoding='utf-8') as tmpl:
 		return tmpl.read().format(node=this_node)
 
 
-@app.route('/tmpl/<path:path>')
+@app.route('/assets/<path:path>')
 def send_static(path):
-    return send_from_directory('tmpl', path)
+	"""Serve static files of the presentation. For production this should go to ngix with proper CGI/uWSGI...."""
+	return send_from_directory('tmpl/assets', path)
 
 
-this_node = socket.getfqdn()
+
+def random_string(letters: list, length=8):
+    """Generate a random string of fixed length"""
+    return ''.join(random.sample(letters, length))
+
+
+# Initialize instance id - just a random hexstring. This makes it possible to observe load balancing to multiple instances
+this_node = random_string(string.hexdigits, 4)
 if __name__ == '__main__':
     app.run(debug=False, host='0.0.0.0')
